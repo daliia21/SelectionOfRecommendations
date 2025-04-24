e08448c15e04b20c52af9b233466616a8644f26d
Dalia <daliasabirova6@gmail.com>
2025-04-24 22:17:44 +0300

    добавлен проект unit тестов

---
+++ SelectionOfRecomendationTests/UnitTest1.cs
@@ -0,0 +1,95 @@
+using Microsoft.EntityFrameworkCore;
+using TouristRoutes;
+using TouristRoutes.Models;
+
+namespace SelectionOfRecomendationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test()
+        {
+
+            AppDbContext dbContext = new AppDbContext();
+
+            //var Dalia = new AppUser
+            //{
+            //    FirstName = "Dalia",
+            //    LastName = "Sabirova",
+            //    Email = "daliasabirova6@gmail.com",
+            //    PasswordHash = "12345"
+            //};
+
+            //// Добавление 2 пользователей и анкету для одного из них
+            //dbContext.Users.Add(Dalia);
+
+            //dbContext.Users.Add(new AppUser
+            //{
+            //    FirstName = "Kamil",
+            //    LastName = "Sabirov",
+            //    Email = "kamil6@gmail.com",
+            //    PasswordHash = "543231"
+            //});
+
+            //dbContext.UserInfos.Add(new AppUserInfo
+            //{
+            //    AgeCategory = "от 12 до 20",
+            //    AppUser = Dalia,
+            //    Budget = "45000",
+            //    DurationOfTheTrip = "Очень опытный",
+            //    FrequentLocation = "Очень опытный",
+            //    LevelOfTraining = "Очень опытный",
+            //    Season = "луто",
+            //    TypeOfTourism = "Очень опытный"
+            //});
+
+            //dbContext.SaveChanges();
+
+
+
+
+            // Нужно извлечь пользователя с id 3 без AppUserInfo
+
+            //var user = dbContext.Users.Find(3);
+
+
+
+
+            // Нужно извлечь пользователя с id 3 с AppUserInfo
+
+            //var user = dbContext.Users
+            //    .Where(u => u.Id == 4)
+            //    .Include(u => u.AppUserInfo)
+            //    .FirstOrDefault();
+
+
+            // Удаления пользователя с id равным 3
+
+            //var user = dbContext.Users.Find(3);
+
+            //dbContext.Users.Remove(user);
+            //dbContext.SaveChanges();
+            
+
+            //var user = dbContext.Users
+            //    .Where(u => u.Id == 4)
+            //    .Include(u => u.AppUserInfo)
+            //    .FirstOrDefault();
+
+            //if (user == null)
+            //{
+            //    return;
+            //}
+
+            //if (user.AppUserInfo != null)
+            //{
+            //    dbContext.UserInfos.Remove(user.AppUserInfo);
+            //}
+
+            //dbContext.Users.Remove(user);
+            //dbContext.SaveChanges();
+
+            // Удаление всех пользователей
+        }
+    }
+}
