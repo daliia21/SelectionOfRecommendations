bd86468399863e0a99d83af6dfb3fd3b68a05fd4
Dalia <daliasabirova6@gmail.com>
2025-04-24 23:30:00 +0300

    Добавлен функционал входа и регистрации формы

--- TouristRoutes/Form1.cs
+++ TouristRoutes/Form1.cs
@@ -1,10 +1,17 @@
+using TouristRoutes.Dtos;
+using TouristRoutes.Services;
+
 namespace TouristRoutes
 {
     public partial class Form1 : Form
     {
+        AccountService _accountService;
+
         public Form1()
         {
             InitializeComponent();
+
+            _accountService = new AccountService();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -16,12 +23,47 @@ namespace TouristRoutes
 
         private void logIn_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Вход успешно выполнен!");
+            LoginDto loginDto = new LoginDto
+            {
+                Email = textBox5.Text,
+                Password = textBox6.Text
+            };
+
+            var result = _accountService.Login(loginDto);
+
+            if (result.Item1)
+            {
+                MessageBox.Show("Аутентификация прошла успешно!");
+
+            }
+            else
+            {
+                MessageBox.Show(result.Item2);
+            }
+
+            
         }
 
         private void registration_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Вы успешно зарегистрировались!");
+            RegisterDto registerDto = new RegisterDto
+            {
+                FirstName = textBox1.Text,
+                LastName = textBox4.Text,
+                Email = textBox3.Text,
+                Password = textBox2.Text
+            };
+
+            var result = _accountService.Register(registerDto);
+
+            if (result.Item1)
+            {
+                MessageBox.Show("Регистрация прошла успешно!");
+
+            } else
+            {
+                MessageBox.Show(result.Item2);
+            }
         }
     }
 }
